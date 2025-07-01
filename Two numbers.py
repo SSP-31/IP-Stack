24c52264a356d285a43aef7df0ccebd0be8d9ad5
Shivani S Patil <130072530+SSP-31@users.noreply.github.com>
2025-07-01 22:54:11 +0530

    Updated the code in the python file

--- Two numbers.py
+++ Two numbers.py
@@ -28,3 +28,21 @@ Constraints:
 Only one valid answer exists.
 
 
+Code in Python:
+class Solution:
+    def twoSum(self, nums, target):
+        lookup = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in lookup:
+                return [lookup[complement], i]
+            lookup[num] = i
+
+# Test cases
+sol = Solution()
+print(sol.twoSum([2,7,11,15], 9))    # Output: [0, 1]
+print(sol.twoSum([3,2,4], 6))        # Output: [1, 2]
+print(sol.twoSum([3,3], 6))          # Output: [0, 1]
+
+
+
